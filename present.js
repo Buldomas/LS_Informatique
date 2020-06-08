173ddd5dae12baf04d7a7c97b5b9b6b6e732fb3a
Buldomas <laurent.soubigou445@orange.fr>
2020-06-08 09:33:59 +0200

    Version1.0

---
+++ present.js
@@ -0,0 +1,27 @@
+var monBouton = document.getElementById("btn1");
+monBouton.addEventListener("click", function () {
+  var btmenu = document.getElementById("collapseOne");
+  btmenu.style.display = "block";
+  var btmenu = document.getElementById("collapseTwo");
+  btmenu.style.display = "none";
+  var btmenu = document.getElementById("collapseThree");
+  btmenu.style.display = "none";
+});
+var monBouton = document.getElementById("btn2");
+monBouton.addEventListener("click", function () {
+  var btmenu = document.getElementById("collapseOne");
+  btmenu.style.display = "none";
+  var btmenu = document.getElementById("collapseTwo");
+  btmenu.style.display = "block";
+  var btmenu = document.getElementById("collapseThree");
+  btmenu.style.display = "none";
+});
+var monBouton = document.getElementById("btn3");
+monBouton.addEventListener("click", function () {
+  var btmenu = document.getElementById("collapseOne");
+  btmenu.style.display = "none";
+  var btmenu = document.getElementById("collapseTwo");
+  btmenu.style.display = "none";
+  var btmenu = document.getElementById("collapseThree");
+  btmenu.style.display = "block";
+});
